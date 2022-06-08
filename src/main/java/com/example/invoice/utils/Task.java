e29c48e1b657c347110869ebaf82dd870c865a84
SeiFu <fxf1517@163.com>
2022-06-08 17:48:08 +0800

    啦啦啦2

--- src/main/java/com/example/invoice/utils/Task.java
+++ src/main/java/com/example/invoice/utils/Task.java
@@ -19,6 +19,8 @@ public class Task extends Thread {
                 System.out.println("abc");
                 System.out.println("efg");
                 System.out.println("abc");
+                System.out.println("abc");
+                System.out.println("abc");
                 break;
             }
         }
