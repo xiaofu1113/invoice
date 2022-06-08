7cea8ebfc9f626d76de275044407ff30404c8c0b
SeiFu <fxf1517@163.com>
2022-06-08 17:01:12 +0800

    啦啦啦

--- src/main/java/com/example/invoice/InvoiceApplication.java
+++ src/main/java/com/example/invoice/InvoiceApplication.java
@@ -1,6 +1,7 @@
 package com.example.invoice;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,10 +11,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableEncryptableProperties
 @SpringBootApplication
+@Slf4j
 public class InvoiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InvoiceApplication.class, args);
+        log.info("InvoiceApplication start success");
     }
 
 }
