56e5ee4d8311f92eafe577b176bb6a6c1fad33ad
SeiFu <fxf1517@163.com>
2022-06-09 22:25:12 +0800

    测试7777

--- src/main/java/com/example/invoice/utils/ServletUtils.java
+++ src/main/java/com/example/invoice/utils/ServletUtils.java
@@ -43,8 +43,6 @@ public class ServletUtils {
         System.out.println(4);
         System.out.println(5);
         System.out.println(6);
-        System.out.println(7);
-        System.out.println(8);
-        System.out.println(9);
+        System.out.println(123);
     }
 }
